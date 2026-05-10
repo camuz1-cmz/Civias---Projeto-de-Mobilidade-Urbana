81d1c8f188d1371048f4910dd8a4bbf430b620e9
camuz1 <73593000+camuz1-cmz@users.noreply.github.com>
2026-05-10 13:05:48 -0300

    Update Firebase configuration and initialization

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,14 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyB0h7aPP7_iQD-He2poXUtioSfdWWc9Zbc",
   authDomain: "civias-e511f.firebaseapp.com",
   projectId: "civias-e511f",
-  storageBucket: "civias-e511f.firebasestorage.app",
+  storageBucket: "civias-e511f.appspot.com",
   messagingSenderId: "579079092420",
   appId: "1:579079092420:web:1b6566c15b40bec6cdb191"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
