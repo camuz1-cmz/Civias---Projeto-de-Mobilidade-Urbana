1afffebd81bc81a53288f7b8692e8f7271a6a89d
camuz1 <73593000+camuz1-cmz@users.noreply.github.com>
2026-05-10 13:06:51 -0300

    Update Firebase initialization and Firestore access

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,11 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB0h7aPP7_iQD-He2poXUtioSfdWWc9Zbc",
   authDomain: "civias-e511f.firebaseapp.com",
   projectId: "civias-e511f",
-  storageBucket: "civias-e511f.appspot.com",
+  storageBucket: "civias-e511f.firebasestorage.app",
   messagingSenderId: "579079092420",
   appId: "1:579079092420:web:1b6566c15b40bec6cdb191"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
