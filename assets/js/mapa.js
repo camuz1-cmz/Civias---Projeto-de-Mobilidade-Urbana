b9c7f8e4dc9970d1e53ff3d0ec5f494517292ba6
camuz1 <73593000+camuz1-cmz@users.noreply.github.com>
2026-05-10 11:34:26 -0300

    Adicionando LeaFlet com pins fictícios

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -4,3 +4,23 @@
 // vindos do banco de dados com as ocorrências reais.
 
 console.log('Mapa do Bairro carregado.');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const map = L.map('map').setView([-20.3155, -40.3128], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap'
+  }).addTo(map);
+
+  // Pins de exemplo
+  L.marker([-20.3155, -40.3128])
+    .addTo(map)
+    .bindPopup("Buraco na via");
+
+  L.marker([-20.3220, -40.3000])
+    .addTo(map)
+    .bindPopup("Asfalto renovado");
+
+});
