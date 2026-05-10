987bfdefe3b9c2d2ff8a427bcb34c02e9e79baae
camuz1 <73593000+camuz1-cmz@users.noreply.github.com>
2026-05-10 11:17:52 -0300

    Leaflet map na home.js
    Added Leaflet map initialization to home.js.

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,3 +3,10 @@
 // Exibe o nome do usuário salvo na sessão
 const userName = sessionStorage.getItem('civias_user') || 'User';
 document.getElementById('userName').textContent = userName;
+
+// Leaflat - MAPA
+const map = L.map('map').setView([-20.3155, -40.3128], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap'
+}).addTo(map);
